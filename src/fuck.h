45e7774ca02179bfce64d80374ab17826fb1551a
esrrhs <esrrhs@localhost>
2014-09-18 16:30:21 +0800

    ??

---
+++ src/fuck.h
@@ -0,0 +1,102 @@
+#pragma once
+
+#include <string>
+#include <list>
+#include <stdint.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <iostream>
+#include <string>
+#include <cstdlib>
+#include <cstring>
+#include <typeinfo>
+#include <stdio.h>
+#include "types.h"
+#include "myflexer.h"
+#include "fuckscript.h"
+#include "compiler.h"
+#include "binary.h"
+#include "paramstack.h"
+#include "interpreter.h"
+
+struct fuck
+{
+    fuck() : errorno(0), mf(this), bin(this), mc(this, &bin), inter(this)
+    {
+    }
+    ~fuck()
+    {
+    }
+
+    // 清空
+    void clear()
+    {
+        clearerr();
+        mf.clear();
+        bin.clear();
+        mc.clear();
+        ps.clear();
+        inter.clear();
+    }
+    
+    void clearerr()
+    {
+        errorno = 0;
+        errorstr[0] = 0;
+    }
+    
+    int errorno;
+    char errorstr[64];
+    
+    // 配置
+    fuckconfig cfg;
+
+    // 解析
+    myflexer mf;
+
+    // 二进制
+    binary bin;
+    
+    // 编译
+    compiler mc;
+
+    // c的参数栈
+    paramstack ps;
+
+    // 当前线程解释器
+    interpreter inter;
+
+    // TODO 异步线程的运行环境
+};
+
+template <typename T>
+T * fknew(fuck * fk)
+{
+    T * t = (T *)fk->cfg.fkm(sizeof(T));
+    new (t) T();
+    return t;
+}
+
+template <typename T, typename P1>
+T * fknew(fuck * fk, P1 p1)
+{
+    T * t = (T *)fk->cfg.fkm(sizeof(T));
+	if (!t)
+	{
+		return 0;
+	}
+    new (t) T(p1);
+    return t;
+}
+
+template <typename T>
+void fkdelete(fuck * fk, T * p)
+{
+	if (p)
+	{
+		p->~T();
+		fk->cfg.fkf(p);
+	}
+}
+
---
+++ src/paramstack.h
@@ -0,0 +1,53 @@
+#pragma once
+
+#include "types.h"
+#include "variant.h"
+
+#define REAL_MAX_FUCK_PARAM_NUM (MAX_FUCK_PARAM_NUM + MAX_FUCK_RETURN_NUM)
+
+struct paramstack
+{
+    force_inline void clear()
+    {
+        m_variant_list_num = 0;
+    }
+    
+    force_inline void push(const variant & v)
+    {
+    	assert(m_variant_list_num < REAL_MAX_FUCK_PARAM_NUM);
+    	m_variant_list[m_variant_list_num] = v;
+    	m_variant_list_num++;
+    }
+
+    force_inline void pop(variant & v)
+    {
+    	assert(m_variant_list_num > 0);
+    	v = m_variant_list[m_variant_list_num - 1];
+    	m_variant_list_num--;
+    }
+
+	force_inline const variant & operator[](int i)
+	{
+		assert(i >= 0 && i < REAL_MAX_FUCK_PARAM_NUM);
+		return m_variant_list[i];
+	}
+
+	force_inline size_t size() const
+	{
+		return m_variant_list_num;
+	}
+
+	variant m_variant_list[REAL_MAX_FUCK_PARAM_NUM];
+	size_t m_variant_list_num;
+};
+
+#define PS_PUSH_AND_GET(ps, v) \
+    assert(ps.m_variant_list_num < REAL_MAX_FUCK_PARAM_NUM);\
+    v = &ps.m_variant_list[ps.m_variant_list_num];\
+    ps.m_variant_list_num++;
+    
+#define PS_POP_AND_GET(ps, v) \
+    assert(ps.m_variant_list_num > 0);\
+    v = &ps.m_variant_list[ps.m_variant_list_num - 1];\
+    ps.m_variant_list_num--;
+    
