ab7f421a8526f2a971b61f7d6e5d51f6b4c2aab5
esrrhs <esrrhs@localhost>
2014-09-20 17:56:50 +0800

    adda

--- src/variant.cpp
+++ src/variant.cpp
@@ -5,11 +5,8 @@
 extern "C" void v_set_real()
 {
     variant v1;
-    variant v2;
-    variant v3;
-    USE(v1);
-    USE(v2);
-    USE(v3);
+    v1.type = variant::REAL;
+    v1.data.real = (double)1.154151511251235f;
 }
 
 void cheat_complie()
