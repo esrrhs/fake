9ea2231aeaa38746efb966ee7b8b83c5ee9b8043
esrrhs <esrrhs@localhost>
2014-09-20 17:57:33 +0800

    sss

---
+++ src/binary.cpp
@@ -0,0 +1,161 @@
+#include "binary.h"
+#include "fuck.h"
+
+const char * OpCodeStr(int opcode)
+{
+    switch (opcode)
+    {
+#define OPCODE_DEF(op)\
+    case OPCODE_##op:\
+        return #op;
+        
+    OPCODE_DEF(ASSIGN)
+
+    OPCODE_DEF(PLUS)
+    OPCODE_DEF(MINUS)
+    OPCODE_DEF(MULTIPLY)
+	OPCODE_DEF(DIVIDE)
+	OPCODE_DEF(DIVIDE_MOD)
+
+    OPCODE_DEF(PLUS_ASSIGN)
+    OPCODE_DEF(MINUS_ASSIGN)
+    OPCODE_DEF(MULTIPLY_ASSIGN)
+	OPCODE_DEF(DIVIDE_ASSIGN)
+	OPCODE_DEF(DIVIDE_MOD_ASSIGN)
+
+	OPCODE_DEF(RETURN)
+	
+	OPCODE_DEF(JNE)
+	OPCODE_DEF(JMP)
+	
+    OPCODE_DEF(AND)
+    OPCODE_DEF(OR)
+    OPCODE_DEF(LESS)
+	OPCODE_DEF(MORE)
+	OPCODE_DEF(EQUAL)
+	OPCODE_DEF(MOREEQUAL)
+	OPCODE_DEF(LESSEQUAL)
+	OPCODE_DEF(NOTEQUAL)
+	
+#undef OPCODE_DEF
+    }
+    return "unknow";
+}
+
+String func_binary::dump() const
+{
+    String ret;
+
+    // 名字
+    ret += "\n[";
+    ret += m_name;
+    ret += "]\n";
+
+    // 最大栈
+    ret += "\tmaxstack:\t";
+    ret += fkitoa(m_maxstack);
+    ret += "\n\n";
+    
+    // 常量表
+    ret += "\t////// const define ";
+    ret += fkitoa(m_const_list_num);
+    ret += " //////\n";
+    for (int i = 0; i < (int)m_const_list_num; i++)
+    {  
+        ret += "\t[";
+        ret += fkitoa(i);
+        ret += "]\t";
+        switch (m_const_list[i].type)
+        {
+        case variant::REAL:
+            ret += "REAL";
+            break;
+        case variant::STRING:
+            ret += "STRING";
+            break;
+        case variant::POINTER:
+            ret += "POINTER";
+            break;
+        default:
+            ret += "unknow";
+            break;
+        }
+        ret += "\t";
+        ret += vartostring(&m_const_list[i]);
+        ret += "\n";
+    }
+    
+    ret += "\n\t////// byte code ";
+    ret += fkitoa(m_size);
+    ret += " //////\n";
+    // 字节码
+    for (int i = 0; i < (int)m_size; i++)
+    {    
+        command cmd = m_buff[i];
+        int type = COMMAND_TYPE(cmd);
+        int code = COMMAND_CODE(cmd);
+        ret += "\t[";
+        ret += fkitoa(i);
+        ret += "]\t";
+        ret += fkxtoa(cmd);
+        ret += "\t";
+        switch (type)
+        {
+        case COMMAND_OPCODE:
+            {
+                ret += "[";
+                ret += OpCodeStr(code);
+                ret += "]\t";
+            }
+            break;
+        case COMMAND_ADDR:
+            {
+                ret += "[ ADDR ]\t";
+                int addrtype = HIINT16(code);
+                int pos = LOINT16(code);
+                switch (addrtype)
+                {
+                case ADDR_STACK:
+                    ret += "STACK";
+                    break;
+                case ADDR_CONST:
+                    ret += "CONST";
+                    break;
+                default:
+                    ret += "unknow ";
+                    ret += fkitoa(addrtype);
+                }
+                ret += "\t";
+                ret += fkitoa(pos);
+            }
+            break;
+        case COMMAND_POS:
+            {
+                ret += "[ POS  ]\t";
+                ret += fkitoa(code);
+            }
+            break;
+        default:
+            {
+                ret += "[unknow]\t";
+            }
+            break;
+        }
+        ret += "\n";
+    }
+    ret += "\n";
+    return ret;
+}
+
+String binary::dump() const
+{
+    String ret;
+    for (int i = 0; i < (int)m_func_list.size(); i++)
+    {
+        const func_binary & bin = m_func_list[i];
+        ret += bin.dump();
+    }
+    return ret;
+}
+    
+
---
+++ src/codegen.h
@@ -0,0 +1,126 @@
+#pragma once
+
+#include "types.h"
+#include "binary.h"
+#include "variant.h"
+
+struct block_identifiers
+{
+    block_identifiers() : name(""), pos(0) {}
+    block_identifiers(String _name, int _pos) : name(_name), pos(_pos) {}
+    String name;
+    int pos;
+};
+
+typedef std::vector<block_identifiers> block_identifiers_list;
+typedef std::vector<block_identifiers_list> block_identifiers_stack;
+typedef std::vector<command> byte_code_list;
+
+struct fuck;
+class codegen
+{
+public:
+    codegen(fuck * fk) : m_fk(fk)
+    {
+        clear();
+    }
+    ~codegen() {}
+
+    void clear()
+    {
+        m_stackpos = 0;
+        m_maxstackpos = 0;
+        m_block_identifiers_stack.clear();
+        m_byte_code_list.clear();
+    }
+    
+    int add_stack_identifier(const String & name);
+
+    int alloc_stack_identifier()
+    {
+        assert(m_block_identifiers_stack.size() > 0);
+        int ret = m_stackpos;
+        block_identifiers_list & list = m_block_identifiers_stack.back();
+        list.push_back(block_identifiers("", m_stackpos));
+        m_stackpos++;
+        if (m_stackpos > m_maxstackpos)
+        {
+            m_maxstackpos = m_stackpos;
+        }
+        return ret;
+    }
+
+    void push_stack_identifiers()
+    {
+        m_block_identifiers_stack.push_back(block_identifiers_list());
+    }
+    
+    void pop_stack_identifiers()
+    {
+        assert(m_block_identifiers_stack.size() > 0);
+        block_identifiers_list & list = m_block_identifiers_stack.back();
+        int stacksize = list.size();
+        m_block_identifiers_stack.pop_back();
+        m_stackpos -= stacksize;
+        assert(m_stackpos >= 0);
+    }
+
+    int getvariable(const String & name)
+    {
+        assert(m_block_identifiers_stack.size() > 0);
+        // ´ÓÏÂÍùÉÏÕÒ
+        for (int i = (int)m_block_identifiers_stack.size() - 1; i >= 0; i--)
+        {
+            block_identifiers_list & list = m_block_identifiers_stack[i];
+            for (int j = 0; j < (int)list.size(); j++)
+            {
+                if (name == list[j].name)
+                {
+                    return list[j].pos;
+                }
+            }
+        }
+        return -1;
+    }
+
+	int getconst(const variant & v);
+
+    void push(uint64_t code)
+    {
+        m_byte_code_list.push_back(code);
+    }
+    void set(int pos, uint64_t code)
+    {
+        assert(pos >= 0 && pos < (int)m_byte_code_list.size());
+        m_byte_code_list[pos] = code;
+    }
+    size_t byte_code_size() const
+    {
+        return m_byte_code_list.size();
+    }
+
+	void output(const String & name, func_binary * bin);
+    
+    int get_cur_variable_pos(const String & name)
+    {
+        assert(m_block_identifiers_stack.size() > 0);
+        block_identifiers_list & list = m_block_identifiers_stack.back();
+        for (int i = 0; i < (int)list.size(); i++)
+        {
+            if (name == list[i].name)
+            {
+                return list[i].pos;
+            }
+        }
+        return -1;
+    }
+    
+private:
+    fuck * m_fk;
+    int m_stackpos;
+    int m_maxstackpos;
+    block_identifiers_stack m_block_identifiers_stack;
+	byte_code_list m_byte_code_list;
+	const_list m_const_list;
+};
+
