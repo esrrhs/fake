3ce5efe5b7ccc053dd752a5fcaa4a5c7ea36f631
esrrhs <esrrhs@localhost>
2014-11-12 22:12:02 +0800

    asdasd

---
+++ src/fake.h
@@ -0,0 +1,132 @@
+#pragma once
+
+#include <string>
+#include <list>
+#include <stdint.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <iostream>
+#include <string>
+#include <cstdlib>
+#include <cstring>
+#include <typeinfo>
+#include <stdio.h>
+#include "types.h"
+#include "myflexer.h"
+#include "fakescript.h"
+#include "compiler.h"
+#include "binary.h"
+#include "paramstack.h"
+#include "interpreter.h"
+#include "native.h"
+#include "assembler.h"
+#include "machine.h"
+#include "stringheap.h"
+#include "bindfunc.h"
+#include "buildinfunc.h"
+
+struct fake
+{
+    fake() : errorno(0), mf(this), bin(this), mc(this, &bin), nt(this), as(this, &nt), inter(this), sh(this), mac(this), bf(this), bif(this)
+    {
+    }
+    ~fake()
+    {
+    }
+
+    // 清空
+    void clear()
+    {
+        clearerr();
+        mf.clear();
+        bin.clear();
+        mc.clear();
+        nt.clear();
+        as.clear();
+        ps.clear();
+        inter.clear();
+        sh.clear();
+        mac.clear();
+        bf.clear();
+        bif.clear();
+    }
+    
+    void clearerr()
+    {
+        errorno = 0;
+        errorstr[0] = 0;
+    }
+    
+    int errorno;
+    char errorstr[64];
+    
+    // 配置
+    fakeconfig cfg;
+
+    // 解析
+    myflexer mf;
+
+    // 二进制
+    binary bin;
+    
+    // 编译
+    compiler mc;
+
+    // 本地jit代码
+    native nt;
+
+    // 汇编器
+    assembler as;
+
+    // c的参数栈
+    paramstack ps;
+
+    // 当前线程解释器
+    interpreter inter;
+
+    // 字符串集合
+    stringheap sh;
+
+    // 当前线程本地环境
+    machine mac;
+
+    // 绑定C函数集合
+    bindfunc bf;
+
+    // 内建的函数集合
+    buildinfunc bif;
+
+    // TODO 异步线程的运行环境
+};
+
+template <typename T>
+T * fknew(fake * fk)
+{
+    T * t = (T *)fk->cfg.fkm(sizeof(T));
+    new (t) T();
+    return t;
+}
+
+template <typename T, typename P1>
+T * fknew(fake * fk, P1 p1)
+{
+    T * t = (T *)fk->cfg.fkm(sizeof(T));
+	if (!t)
+	{
+		return 0;
+	}
+    new (t) T(p1);
+    return t;
+}
+
+template <typename T>
+void fkdelete(fake * fk, T * p)
+{
+	if (p)
+	{
+		p->~T();
+		fk->cfg.fkf(p);
+	}
+}
+
---
+++ src/types.cpp
@@ -0,0 +1,122 @@
+#include "types.h"
+#include "bison.h"
+#include "fake.h"
+#include "variant.h"
+#include "paramstack.h"
+
+void fklog(const char * header, const char * file, const char * func, int pos, const char *fmt, ...)
+{
+	FILE *pLog = NULL;
+	time_t clock1;
+	struct tm * tptr;
+	va_list ap;
+	
+	pLog = fopen("fakescript.log", "a+");
+	if (pLog == NULL)
+	{
+		return;
+	}
+	
+	clock1 = time(0);
+	tptr = localtime(&clock1);
+
+	fprintf(pLog, "===========================[%d.%d.%d, %d.%d.%d]%s:%d,%s:===========================\n%s", 
+		tptr->tm_year+1990,tptr->tm_mon+1,
+		tptr->tm_mday,tptr->tm_hour,tptr->tm_min,
+		tptr->tm_sec,file,pos,func,header);
+
+	va_start(ap, fmt);
+	vfprintf(pLog, fmt, ap);
+	fprintf(pLog, "\n\n");
+	va_end(ap);
+
+	fclose(pLog);
+}
+
+String fkget_token_name(int token)
+{
+#define TOKEN_SWITCH(x) case x: return #x;
+    switch (token)
+    {
+        TOKEN_SWITCH(VAR_BEGIN)
+        TOKEN_SWITCH(RETURN)
+        TOKEN_SWITCH(BREAK)
+        TOKEN_SWITCH(FUNC)
+        TOKEN_SWITCH(WHILE)
+        TOKEN_SWITCH(TRUE)
+        TOKEN_SWITCH(FALSE)
+        TOKEN_SWITCH(IF)
+        TOKEN_SWITCH(THEN)
+        TOKEN_SWITCH(ELSE)
+        TOKEN_SWITCH(END)
+        TOKEN_SWITCH(STRING_DEFINITION)
+        TOKEN_SWITCH(IDENTIFIER)
+        TOKEN_SWITCH(NUMBER)
+        TOKEN_SWITCH(SINGLE_LINE_COMMENT)
+        TOKEN_SWITCH(DIVIDE_MOD)
+        TOKEN_SWITCH(ARG_SPLITTER)
+        TOKEN_SWITCH(PLUS)
+        TOKEN_SWITCH(MINUS)
+        TOKEN_SWITCH(DIVIDE)
+        TOKEN_SWITCH(MULTIPLY)
+        TOKEN_SWITCH(ASSIGN)
+        TOKEN_SWITCH(MORE)
+        TOKEN_SWITCH(LESS)
+        TOKEN_SWITCH(MORE_OR_EQUAL)
+        TOKEN_SWITCH(LESS_OR_EQUAL)
+        TOKEN_SWITCH(EQUAL)
+        TOKEN_SWITCH(NOT_EQUAL)
+        TOKEN_SWITCH(OPEN_BRACKET)
+        TOKEN_SWITCH(CLOSE_BRACKET)
+        TOKEN_SWITCH(FLOAT)
+        TOKEN_SWITCH(PLUS_ASSIGN)
+        TOKEN_SWITCH(MINUS_ASSIGN)
+        TOKEN_SWITCH(DIVIDE_ASSIGN)
+        TOKEN_SWITCH(MULTIPLY_ASSIGN)
+        TOKEN_SWITCH(DIVIDE_MOD_ASSIGN)
+        default:
+            return fkitoa(token);
+    }
+    #undef TOKEN_SWITCH
+}
+
+void * safe_fkmalloc(fake * fk, size_t size)
+{
+    if (fk && size)
+    {
+        return fk->cfg.fkm(size);
+    }
+    return 0;
+}
+
+void safe_fkfree(fake * fk, void * p)
+{
+    if (fk && p)
+    {
+        fk->cfg.fkf(p);
+    }
+}
+
+void seterror(fake * fk, efkerror err, const char *fmt, ...)
+{
+    fk->errorno = err;
+	va_list ap;
+	va_start(ap, fmt);
+    vsnprintf(fk->errorstr, sizeof(fk->errorstr) - 1, fmt, ap);
+	va_end(ap);
+	fk->errorstr[sizeof(fk->errorstr) - 1] = 0;
+}
+
+String vartostring(const variant * v)
+{
+    String s;
+    V_TOSTRING(v, s);
+    return s;
+}
+
+paramstack * getps(fake * fk)
+{
+    return &fk->ps;
+}
+
+
